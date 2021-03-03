c166e009dd8766b4dfb49fe9cf8714f473903d7e
Andrew <ahernandez@codeforamerica.org>
2021-03-02 22:01:18 -0800

    got file upload working for SNAP and FRL

--- mobileVers/application/urls.py
+++ mobileVers/application/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
-
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 # Need to specify my urls to be used within this project
@@ -14,6 +14,5 @@ urlpatterns = [
 
     # Available/NotAvailable Digital equity in your area
     path('available', views.available, name='available'),
-    path('notAvailable', views.notAvailable, name='notAvailable'),
-
-]
+    path('notAvailable', views.notAvailable, name='notAvailable')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #this is needed to get file uploads to work! 
