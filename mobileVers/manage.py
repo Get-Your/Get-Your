c48425bbf7edf6b87db53b9f79a5ce249de22841
Andrew <ahernandez@codeforamerica.org>
2021-03-11 14:28:17 -0800

    changed settings back!

--- mobileVers/manage.py
+++ mobileVers/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobileVers.settings.alocal')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobileVers.settings') #.alocal put this in to make python manage.py [] easier, don't need to use --settings=...
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
