da35935785cd22b167302caa3d28ac6efa18ad1c
Andrew <ahernandez@codeforamerica.org>
2021-04-05 14:12:07 -0700

    manual taxbox input done!

--- mobileVers/dashboard/models.py
+++ mobileVers/dashboard/models.py
@@ -24,7 +24,7 @@ class Form(TimeStampedModel):
         choices=form_titles,
     )
     document = models.FileField()
-    TaxBox1Amount = models.CharField(max_length=8,)
+    TaxBoxAmount = models.DecimalField(max_digits=9, blank=True, decimal_places=2,)
 
 
 
