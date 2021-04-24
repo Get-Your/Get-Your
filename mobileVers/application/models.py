3cd707ea8c5cce21f1eaac6fe5e48b1c53feb52a
Andrew <ahernandez@codeforamerica.org>
2021-04-24 14:05:44 -0700

    included UI update removed more 1040 comments

--- mobileVers/application/models.py
+++ mobileVers/application/models.py
@@ -148,7 +148,6 @@ class programs(TimeStampedModel):
     # TODO: Andrew/Grace - These two fields have to be entered in after the verification of the documents
     snap = models.BooleanField()
     freeReducedLunch = models.BooleanField()
-    #Tax1040 = models.BooleanField()
 
 class zipCode(TimeStampedModel):
     zipCode = models.DecimalField(max_digits=5, decimal_places=0)    
