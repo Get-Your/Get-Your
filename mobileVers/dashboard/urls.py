e38c4e31acfeab4fae1769e1fd8e733dc52bee18
Andrew <ahernandez@codeforamerica.org>
2021-09-13 08:36:03 -0700

    settings page implemented

--- mobileVers/dashboard/urls.py
+++ mobileVers/dashboard/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('qualifiedPrograms', views.qualifiedPrograms, name='qualifiedPrograms'),
     path('ProgramsList', views.ProgramsList, name='ProgramsList'),
     path('FAQ', views.FAQ, name='FAQ'),
+    path('settings', views.settings, name='settings'),
     # Available/NotAvailable Digital equity in your area
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #this is needed to get file uploads to work! 
