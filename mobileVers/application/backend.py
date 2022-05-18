b70f6e431772f7525a7fe64b3cbdad5e7fc5c647
Andrew <ahernandez@codeforamerica.org>
2022-05-17 20:38:10 -0400

    License reference added to relevant source files

--- mobileVers/application/backend.py
+++ mobileVers/application/backend.py
@@ -1,6 +1,9 @@
-'''
-instructions go here
-'''
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version
+"""
 import csv
 from usps import USPSApi, Address
 import re
--- mobileVers/manage.py
+++ mobileVers/manage.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version
+"""
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
