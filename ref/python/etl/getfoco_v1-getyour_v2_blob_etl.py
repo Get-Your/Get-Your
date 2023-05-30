0fac32178126bca8d4b1ff8770fcd3de748c4bc7
Tim Campbell <consulttim@outlook.com>
2023-05-29 20:45:34 -0600

    Blob ETL: Removed intermediary storage account.
    I'm going to use local storage instead of an intermediate account.

--- ref/python/etl/getfoco_v1-getyour_v2_blob_etl.py
+++ ref/python/etl/getfoco_v1-getyour_v2_blob_etl.py
@@ -13,29 +13,25 @@ from pathlib import Path
 import re
 from azure.storage.blob import BlobServiceClient
 from rich.progress import Progress
-from typer import prompt
 
-raise Warning("This script uses the newest version of azure-storage-blob; recommend using a separate venv than Get-Your")
-
-genericProfile = prompt('Enter a generic database profile to use for porting')   
+# Enter a generic database profile to use for porting
+genericProfile = 'getfoco_dev'
+print(f"Beginning blob transfer for '{genericProfile}'")
 
 load_dotenv()
 
 azCopyExe = r'c:\azure\azcopy\azcopy.exe'
 
-# Transfer blobs from source to 'transfer', removing 'media/' from their name
+# Transfer blobs from source to local drive, removing 'media/' from their name
 # and any leading zeros after 'user_' in the process
-# Then transfer back to the 'permanent' account
-intermediaryAccountName = 'tempgetfocostore'
-intermediaryContainerName = 'filetransfer'
-INTERMEDIARYACCOUNT_SAS = os.getenv("TRANSERFILESTORE_SAS")
+# Then transfer back to the storage account
 
 if '_dev' in genericProfile:            
-    permanentAccountName = 'devgetfocofilestore'
-    PERMANENTACCOUNT_SAS = os.getenv("DEVFILESTORE_SAS")
+    storageAccountName = 'devgetfocofilestore'
+    STORAGEACCOUNT_SAS = os.getenv("DEVFILESTORE_SAS")
 else:
-    permanentAccountName = 'getfocofilestore'
-    PERMANENTACCOUNT_SAS = os.getenv("PRODFILESTORE_SAS")
+    storageAccountName = 'getfocofilestore'
+    STORAGEACCOUNT_SAS = os.getenv("PRODFILESTORE_SAS")
 permanentContainerName = 'applicantfiles'
 
 # Define AzCopy parameters
@@ -43,16 +39,16 @@ azCopyExe = r'c:\azure\azcopy\azcopy.exe'
 localDirectory = Path(r'B:\get_foco\fileshare_transfer')
 
 # Connect to 'permanent' blob storage
-perm_blob_service_client = BlobServiceClient(
-    account_url=f'https://{permanentAccountName}.blob.core.usgovcloudapi.net',
-    credential=PERMANENTACCOUNT_SAS,
+blob_service_client = BlobServiceClient(
+    account_url=f'https://{storageAccountName}.blob.core.usgovcloudapi.net',
+    credential=STORAGEACCOUNT_SAS,
     )
-perm_container_client = perm_blob_service_client.get_container_client(
+container_client = blob_service_client.get_container_client(
     container=permanentContainerName,
     )
 
 # Loop through all blobs, using AzCopy to download
-blobList = list(perm_container_client.list_blobs())
+blobList = list(container_client.list_blobs())
 with Progress() as progress:
 
     downloadTask = progress.add_task(
@@ -71,7 +67,7 @@ with Progress() as progress:
             [
                 azCopyExe,
                 'copy',
-                f"https://{permanentAccountName}.blob.core.usgovcloudapi.net/{permanentContainerName}/{blobitm.name}{PERMANENTACCOUNT_SAS}",
+                f"https://{storageAccountName}.blob.core.usgovcloudapi.net/{permanentContainerName}/{blobitm.name}{STORAGEACCOUNT_SAS}",
                 str(localDirectory.joinpath(newName)),
                 ],
             stdout=PIPE,
@@ -94,9 +90,6 @@ with Progress() as progress:
             
         progress.update(downloadTask, advance=1)
         
-        if idxitm >= 2:
-            break
-        
 ## Once all items have been downloaded, upload them to same blob storage with
 # new name and delete them locally
 uploadList = os.listdir(localDirectory)
@@ -118,7 +111,7 @@ with Progress() as progress:
              azCopyExe,
              'copy',
              str(uplPath),
-             f"https://{permanentAccountName}.blob.core.usgovcloudapi.net/{permanentContainerName}{PERMANENTACCOUNT_SAS}",
+             f"https://{storageAccountName}.blob.core.usgovcloudapi.net/{permanentContainerName}{STORAGEACCOUNT_SAS}",
              '--recursive',
              ],
             stdout=PIPE,
