64ea6f6c0e04d0224ac15f3f03c9d0633cbad917
TiCampbell <TiCampbell@fcgov.com>
2023-12-04 11:02:52 -0700

    Reverted log_level from ForeignKey; the level as designated in the logger cannot be a model instance.

--- getyour/log/models.py
+++ getyour/log/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from log.constants import LOG_LEVELS
+
 
 class LevelRD(models.Model):
     name = models.CharField(max_length=20)
@@ -18,11 +20,8 @@ class Detail(models.Model):
 
     app_name = models.CharField(max_length=20, db_index=True)
     logger_name = models.CharField(max_length=100)
-    log_level = models.ForeignKey(
-        LevelRD,
-        related_name='detail',
-        # Don't update these levels if the log_level is deleted
-        on_delete=models.DO_NOTHING,
+    log_level = models.PositiveSmallIntegerField(
+        choices=LOG_LEVELS,
     )
     function = models.CharField(max_length=50, null=True)
     user_id = models.PositiveBigIntegerField(null=True)
