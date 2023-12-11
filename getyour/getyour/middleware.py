06c23f4c33f0fb9c42efe0b02985ae926fc88ca3
TiCampbell <TiCampbell@fcgov.com>
2023-12-10 21:07:45 -0700

    Critical bug fix: Rearranged RenewalModeMiddleware so what_page_renewal is run before the (default) view is called.

--- getyour/getyour/middleware.py
+++ getyour/getyour/middleware.py
@@ -159,21 +159,25 @@ class RenewalModeMiddleware:
         # Code to be executed for each request before
         # the view (and later middleware) are called.
 
-        response = self.get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
-
+        # This runs only for getting into the renewal process (the 'renew'
+        # button on the dashboard, where 'renewal_mode' is specified as a URL
+        # parameter in the GET call)
         if request.GET.get('renewal_mode', False):
             # This session var will be the global bool for renewal_mode
             request.session['renewal_mode'] = True
 
             if request.user.last_renewal_action:
                 what_page = what_page_renewal(request.user.last_renewal_action)
+
                 # Redirect to the what_page designation, if exists (None is when
                 # all pages have been completed)
                 if what_page is not None:
                     return redirect(what_page)
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
             
         return response
 
