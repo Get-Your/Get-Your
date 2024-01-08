2f5bf1973754427451e517fcfd0fb32651519de4
TiCampbell <TiCampbell@fcgov.com>
2024-01-08 08:51:42 -0700

    Added logging to send_renewal_email() task.

--- getyour/app/tasks.py
+++ getyour/app/tasks.py
@@ -1,14 +1,26 @@
 from app.backend import broadcast_renewal_email, check_if_user_needs_to_renew
 from app.models import User
 from app.constants import notification_buffer_month
+from log.wrappers import LoggerWrapper
 
+import logging
 import pendulum
 
+
 def send_renewal_email():
+    # Initialize logger
+    logger = LoggerWrapper(logging.getLogger(__name__))
+
+    logger.debug(
+        "Entering function",
+        function='send_renewal_email',
+    )
+
     # For every user in the database that isn't archived, check if they
     # need to renew their application
     for user in User.objects.filter(is_archived=False):
         needs_renewal = check_if_user_needs_to_renew(user.id)
+
         # Check if the user needs to renew and if they have been notified within
         # the notification buffer period. If they haven't been notified, send
         # them a renewal email.
@@ -19,13 +31,24 @@ def send_renewal_email():
             # (e.g. period.months + period.years*12 = period.in_months())
             months_since_last_notification = (pendulum.now() - user.last_action_notification_at).in_months()
             if months_since_last_notification > notification_buffer_month:
+                logger.info(
+                    "User needs renewal; sending notification",
+                    function='send_renewal_email',
+                    user_id=user.id,
+                )
+                
                 broadcast_renewal_email(user.email)
+
                 # Now update the user's last_action_notification_at
                 # field to the current time
                 user.last_action_notification_at = pendulum.now()
                 user.save()
             else:
-                # TODO: Archive users that haven't renewed and have exceeded the 30 day
-                # notification window
-                continue
-    
+                logger.debug(
+                    "User needs renewal but has recently been notified",
+                    function='send_renewal_email',
+                    user_id=user.id,
+                )
+
+                # TODO: Discuss archiving users that haven't renewed and have
+                # exceeded the 30 day notification window
