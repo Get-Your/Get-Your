d5b4bc4ac70ec5ff189016d843dc3b28f7726fd4
TiCampbell <TiCampbell@fcgov.com>
2024-05-21 07:07:10 -0600

    usercontent: Added logging to view_file.

--- getyour/usercontent/views.py
+++ getyour/usercontent/views.py
@@ -1,7 +1,19 @@
+import logging
+
 from django.shortcuts import render
+
+from logger.wrappers import LoggerWrapper
+
+# Initialize logger
+log = LoggerWrapper(logging.getLogger(__name__))
 
 
 def view_file(request):
+    log.debug(
+        "Entering function",
+        function='view_file',
+    )
+    
     return render(
         request,
         'view_file.html',
