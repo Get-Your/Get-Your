2d7c04e0fa92895d0d83293cbf528221fcae577e
TiCampbell <TiCampbell@fcgov.com>
2024-05-21 07:09:57 -0600

    Changed 'usercontent' app name to 'blobviewer' for clarity.

--- getyour/blobviewer/apps.py
+++ getyour/blobviewer/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UsercontentConfig(AppConfig):
+class BlobviewerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "usercontent"
+    name = "blobviewer"
