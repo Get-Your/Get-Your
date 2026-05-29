3a437b9befb373b6e6775e7eaf28eeab5f3ffb35
Tim Campbell <tim@consulttim.com>
2026-05-29 14:55:00 -0600

    app.forms: Updated ClearableFileInput to use Django 4.2 attribute terminology.

--- getyour/app/forms.py
+++ getyour/app/forms.py
@@ -136,7 +136,7 @@ class FileUploadForm(forms.ModelForm):
         model = EligibilityProgram
         fields = ['id', 'document_path',]
         widgets = {
-            'document_path': forms.ClearableFileInput(attrs={'multiple': True}),
+            'document_path': forms.ClearableFileInput(attrs={'allow_multiple_selected': True}),
         }
 
 
