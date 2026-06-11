a4e2d18a74f05978112edaf53c41cbdd4b3526c1
TiCampbell <TiCampbell@fcgov.com>
2026-06-11 07:50:54 -0600

    backend: Updated gma_lookup to use the new City of Fort Collins endpoint (a drop-in replacement).

--- getyour/app/backend.py
+++ getyour/app/backend.py
@@ -301,7 +301,7 @@ def gma_lookup(coord_string, target_wkid):
 
     """
 
-    url = 'https://gisweb.fcgov.com/arcgis/rest/services/FCMaps/MapServer/26/query'
+    url = 'https://gis.fortcollins.gov/arcgis/rest/services/GMA/MapServer/0/query'
 
     payload = {
         # Manually stringify 'geometry' - requests and json.dumps do this
