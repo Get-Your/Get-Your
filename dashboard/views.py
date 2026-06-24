cda2058fbfa46273442845c6baa9f778a58517f1
scordill <scordill@fcgov.com>
2026-06-24 08:30:49 -0600

    Dashboard view

--- dashboard/views.py
+++ dashboard/views.py
@@ -19,6 +19,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import os
 import json
+import pendulum
 import base64
 import logging
 
@@ -29,7 +30,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.files.storage import default_storage
 
 from get_your.users.models import User
-from ref.models import AddressRef
+from ref.models import AddressRef, IQProgramRef
 from app.models import HouseholdMembers
 from monitor.wrappers import LoggerWrapper
 
@@ -40,13 +41,49 @@ log = LoggerWrapper(logging.getLogger(__name__))
 
 @login_required(redirect_field_name='auth_next')
 def dashboard(request, pk, **kwargs):
-    user = get_object_or_404(User.objects.prefetch_related('householdmembers'), pk=pk)
+    user = get_object_or_404(User.objects.prefetch_related(
+        'householdmembers',
+        'iq_programs',
+        'eligibility_files'
+    ), pk=pk)
+
+    one_year_ago = pendulum.now().subtract(years=1)
+
+    all_available_programs = IQProgramRef.objects.filter(is_active=True).order_by('friendly_name')
+    user_programs_pending_ids = list(user.iq_programs.filter(
+        applied_at__isnull=False,
+        enrolled_at__isnull=True
+    ).values_list(
+        'program_id',
+        flat=True
+    ))
+
+    user_program_ids = list(user.iq_programs.filter(
+        enrolled_at__isnull=False
+    ).values_list(
+        'program_id',flat=True
+    ))
+
+    user_program_renewal_ids = list(user.iq_programs.filter(
+        enrolled_at__lt=one_year_ago,
+        program__renewal_interval_year__isnull=False
+    ).values_list(
+        'program_id',flat=True
+    ))
+
+    all_user_programs = {
+        'pending': user_programs_pending_ids,
+        'enrolled': user_program_ids,
+        'renewals_needed': user_program_renewal_ids
+    }
 
     return render(
             request,
             'dashboard/dashboard.html',
             {
                 'user': user,
+                'all_available_programs': all_available_programs,
+                'all_user_programs': all_user_programs,
                 "title": "Get FoCo Dashboard",
             },
         )
